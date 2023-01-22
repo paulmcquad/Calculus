b58bdef5097742248155beeb7a66132c6fb273c1
Paul McQuade <paulmcquad@gmail.com>
2023-01-22 19:26:03 +0000

    Added Hyperbolic Functions
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Chapters/2-Trigonometry/main_h.m
+++ Chapters/2-Trigonometry/main_h.m
@@ -1,11 +1,25 @@
-figure(1);
+#figure('Trigonometry');
+figure('name','Trigonometry - Sinh vs Cosh');
 x=[-pi:0.01:pi];
-y = tanh(x);
+
+y = sinh(x);
+y2 = cosh(x);
 
 % Plot the signal versus time:
 plot(x,y);
-
+hold on;
+plot(x,y2);
 grid on;
 %xlabel('time (in seconds)');
-title('Tanh');
-legend('tanh')
+title('Hyperbolic - (Sinh vs Cosh)');
+legend('Sinh', 'Cosh')
+
+figure('name','Trigonometry - Tanh');
+
+y3 = tanh(x);
+
+plot(x,y3);
+grid on;
+
+title('Hyperbolic - (Tanh)');
+legend('Tanh')
