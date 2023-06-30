de91514ae227345d35993c918a8d5285d63184fa
Paul McQuade <paulmcquad@gmail.com>
2023-06-30 22:11:54 +0100

    Calculcus_Differentiation
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- Chapters/4-Differentiation/Calculcus_Differentiation.m
+++ Chapters/4-Differentiation/Calculcus_Differentiation.m
@@ -1,4 +1,5 @@
 # Calculcus - Differentiation
+# https://www.youtube.com/watch?v=BcOPKQAZcn0
 # Differentiating gives the gradient of a graph
 
 pkg load symbolic
@@ -14,8 +15,8 @@ f2(x) = 2*x^4-6*x+1
 d2(x) = diff(f2)
 
 # Example of a Polynomial Equation
-#f3(x) = (3*x^2)+(4/x)-2
-#d3(x) = diff(f3)
+f3(x) = 3*x^2+((x/4)**-1)-2
+d3(x) = diff(f3)
 
 # Example of a Polynomial Equation
 #f4(x) = 2*sqrt(x)-(5/x^2)
